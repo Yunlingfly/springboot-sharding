9256113883ca5844a26744bde3f41b71515a1941
yunlingfly <508821881@qq.com>
2020-05-11 16:41:43 +0800

    [DOC] author

--- src/main/java/cn/yunlingfly/springbootsharding/SpringbootShardingApplication.java
+++ src/main/java/cn/yunlingfly/springbootsharding/SpringbootShardingApplication.java
@@ -4,6 +4,13 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 启动类
+ *
+ * @author yunlingfly
+ * @version 1.0
+ * @date 2020-5-11
+ */
 @SpringBootApplication
 @MapperScan(value = {"cn.yunlingfly.springbootsharding.infra.mapper"})
 public class SpringbootShardingApplication {
